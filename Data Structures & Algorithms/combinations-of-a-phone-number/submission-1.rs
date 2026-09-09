42180bd21e37051043a44052092ca5e215c40fb4
andrei <113675008+whoitandrei@users.noreply.github.com>
2026-09-09 14:09:07 +0700

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.rs
@@ -0,0 +1,61 @@
+impl Solution {
+
+    fn backtrack(digits : &str,
+                 ind : usize,
+                 mapping : &[&str; 10],
+                 cur : &mut String,
+                 res : &mut Vec<String>) {
+
+        if ind == digits.len() {
+            res.push(cur.clone());
+            return;
+        }
+
+        let digit = digits.as_bytes()[ind] - b'0';
+        let letters = mapping[digit as usize];
+
+        for letter in letters.chars() {
+            cur.push(letter);
+            Self::backtrack(
+                digits,
+                ind + 1,
+                mapping,
+                cur,
+                res
+            );
+            cur.pop();
+        }
+    }
+
+    pub fn letter_combinations(digits: String) -> Vec<String> {
+        if digits.is_empty() {
+            return Vec::new();
+        }
+
+        let mapping = [
+            "",
+            "",
+            "abc",
+            "def",
+            "ghi",
+            "jkl",
+            "mno",
+            "pqrs",
+            "tuv",
+            "wxyz",
+        ];
+
+        let mut res = Vec::new();
+        let mut cur = String::new();
+
+        Self::backtrack(
+            &digits,
+            0,
+            &mapping,
+            &mut cur,
+            &mut res
+        );
+
+        return res;
+    }
+}
